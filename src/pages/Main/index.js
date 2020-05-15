15782818b7bde2ddcec3d020391288f076f97d68
RodrigoCortezDev <rodrigocortezdev@gmail.com>
2020-05-15 00:23:18 -0300

    Defnições de projeto

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,22 +1,18 @@
 import React from 'react';
-// import { Icon } from 'react-native-vector-icons/MaterialIcons';
+import { Icon } from 'react-native-vector-icons/MaterialIcons';
 import { Text } from 'react-native';
 import { Container, Form, Input, SubmitButton } from './styles';
 
 export default function Main({ navigation }) {
-    return (
-        <Container>
-            <Form>
-                <Input
-                    autoCorrect={false}
-                    autoCapitalize="none"
-                    placeholder="Adicionar Usuário"
-                />
-                <SubmitButton color="#fff" onPress={() => navigation.navigate('Users') }>
-                    <Text>OK</Text>
-                    {/* <Icon name="add" size={20} color="#fff"/> */}
-                </SubmitButton>
-            </Form>
-        </Container>
-    );
+	return (
+		<Container>
+			<Form>
+				<Input autoCorrect={false} autoCapitalize="none" placeholder="Adicionar Usuário" />
+				<SubmitButton color="#fff" onPress={() => navigation.navigate('Users')}>
+					<Text>OK</Text>
+					{/* <Icon name="add" size={20} color="#fff"/> */}
+				</SubmitButton>
+			</Form>
+		</Container>
+	);
 }
