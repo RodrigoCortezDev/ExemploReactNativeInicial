ba82d050c89f2264746e961d66899613c41a7567
RodrigoCortezDev <rodrigocortezdev@gmail.com>
2020-05-15 02:13:13 -0300

    Consulta de API (axios)

--- App.js
+++ App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import Routes from './src/routes';
 import { StatusBar } from 'react-native';
 
-export default function App() {
-  return (
-          <>
-            <StatusBar backgroundColor="#7159c1" barStyle="light-content" />
-            <Routes />
-          </>
-        );
-}
+import './src/config/ReactotronConfig';
 
+export default function App() {
+	return (
+		<>
+			<StatusBar backgroundColor="#7159c1" barStyle="light-content" />
+			<Routes />
+		</>
+	);
+}
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,22 +1,61 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, Alert, Keyboard } from 'react-native';
 import { Container, Form, Input, SubmitButton } from './styles';
-import { Icon } from 'react-native-vector-icons/MaterialIcons';
+import api from '../../services/api';
+
+// import { Icon } from 'react-native-vector-icons/MaterialIcons';
 
 export default class Main extends React.Component {
-	state = {};
+	//Variaveis de estado
+	state = {
+		newUser: '',
+		users: [],
+	};
 
+	//Ao abrir o app
 	componentDidMount() {}
 
+	//Adicionar o usuário
+	handleAddUser = async () => {
+		const { users, newUser } = this.state;
+
+		const response = await api.get(`/users/${newUser}`);
+
+		const data = {
+			name: response.data.name,
+			login: response.data.login,
+			bio: response.data.bio,
+			avatar: response.data.avatar_url,
+		};
+
+		this.setState({ users: [...users, data], newUser: '' });
+		Keyboard.dismiss();
+	};
+
+	//Renderização da tela
 	render() {
+		const { users, newUser } = this.state;
+
 		return (
 			<Container>
 				{/* <Icon name="add" size={20} color="#fff" /> */}
 				<Form>
-					<Input autoCorrect={false} autoCapitalize="none" placeholder="Adicionar Usuário..." />
-					<SubmitButton color="#fff" onPress={() => this.props.navigation.navigate('Users')}>
-						<Text>OK</Text>
+					<Input
+						autoCorrect={false}
+						autoCapitalize="none"
+						placeholder="Adicionar Usuário..."
+						onChangeText={(text) => this.setState({ newUser: text })}
+						returnKeyType="send"
+						onSubmitEditing={this.handleAddUser}
+						value={newUser}
+					/>
+					<SubmitButton rippleColor="#fff" onPress={this.handleAddUser}>
+						<Text>Incuir</Text>
 					</SubmitButton>
+
+					{/* <SubmitButton color="#fff" onPress={() => this.props.navigation.navigate('Users')}>
+						<Text>OK</Text>
+					</SubmitButton> */}
 				</Form>
 			</Container>
 		);
