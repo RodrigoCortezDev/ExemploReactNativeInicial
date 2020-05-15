c612fb079fb2070863adea3b4b533bc562694fe4
RodrigoCortezDev <rodrigocortezdev@gmail.com>
2020-05-15 00:45:44 -0300

    Testes com icones

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -3,16 +3,22 @@ import { Icon } from 'react-native-vector-icons/MaterialIcons';
 import { Text } from 'react-native';
 import { Container, Form, Input, SubmitButton } from './styles';
 
-export default function Main({ navigation }) {
-	return (
-		<Container>
-			<Form>
-				<Input autoCorrect={false} autoCapitalize="none" placeholder="Adicionar Usuário" />
-				<SubmitButton color="#fff" onPress={() => navigation.navigate('Users')}>
-					<Text>OK</Text>
-					{/* <Icon name="add" size={20} color="#fff"/> */}
-				</SubmitButton>
-			</Form>
-		</Container>
-	);
+export default class Main extends React.Component {
+	state = {};
+
+	componentDidMount() {}
+
+	render() {
+		return (
+			<Container>
+				{/* <Icon name="add" size={20} color="#fff" /> */}
+				<Form>
+					<Input autoCorrect={false} autoCapitalize="none" placeholder="Adicionar Usuário..." />
+					<SubmitButton color="#fff" onPress={() => this.props.navigation.navigate('Users')}>
+						<Text>OK</Text>
+					</SubmitButton>
+				</Form>
+			</Container>
+		);
+	}
 }
